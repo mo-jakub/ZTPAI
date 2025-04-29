0a1de2f0968102beafdfe7163bb5660165863ecb
mo-jakub <jakub.mosur@student.pk.edu.pl>
2025-04-29 07:27:52 +0200

    docs(api): add full Swagger documentation for all endpoints

--- backend/src/Controller/BookController.php
+++ backend/src/Controller/BookController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\books;
+use OpenApi\Attributes as OA;
+use App\Service\BookService;
+use App\Entity\Books;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +21,26 @@ class BookController extends AbstractController
     }
 
     #[Route('/api/books', name: 'get_books', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    #[OA\Get(
+        path: '/api/books',
+        summary: 'Returns all books',
+        tags: ['Books'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'List of books',
+                content: new OA\JsonContent(
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/Book')
+                )
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Not authorized',
+            )
+        ]
+    )]
     public function getBooks(): JsonResponse
     {
         $books = $this->entityManager->getRepository(books::class)->findAll();
@@ -36,6 +58,34 @@ class BookController extends AbstractController
     }
 
     #[Route('/api/books/{id}', name: 'get_book_by_id', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    #[OA\Get(
+        path: '/api/books/{id}',
+        parameters: [
+            new OA\Parameter(
+                name: 'id',
+                in: 'path',
+                required: true,
+                description: 'ID of the book',
+                schema: new OA\Schema(type: 'integer')
+            )
+        ],
+        summary: 'Returns a book by ID',
+        tags: ['Books'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Book details',
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/Book'
+                )
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Not authorized',
+            )
+        ]
+    )]
     public function getBookById(int $id): JsonResponse
     {
         $book = $this->entityManager->getRepository(books::class)->find($id);
@@ -55,6 +105,25 @@ class BookController extends AbstractController
     }
 
     #[Route('/api/books', name: 'add_book', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    #[OA\Post(
+        path: '/api/books',
+        summary: 'Add a new book',
+        tags: ['Books'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Book added successfully',
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/Book'
+                )
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Not authorized',
+            )
+        ]
+    )]
     public function addBooks(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -76,6 +145,34 @@ class BookController extends AbstractController
     }
 
     #[Route('/api/books/{id}', name: 'delete_book_by_id', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN')]
+    #[OA\Delete(
+        path: '/api/books',
+        summary: 'Delete a book by ID',
+        parameters: [
+            new OA\Parameter(
+                name: 'id',
+                in: 'path',
+                required: true,
+                description: 'ID of the book',
+                schema: new OA\Schema(type: 'integer')
+            )
+        ],
+        tags: ['Books'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Book deleted successfully',
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/Book'
+                )
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Not authorized',
+            )
+        ]
+    )]
     public function deleteBookById(int $id): JsonResponse
     {
         $book = $this->entityManager->getRepository(books::class)->find($id);
